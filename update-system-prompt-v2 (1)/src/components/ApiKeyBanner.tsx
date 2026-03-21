f60a39e5624411fc3e97bea0c6f5b935b079aca0
Moumita Paul <paulmoumita985@gmail.com>
2026-03-21 23:13:35 +0530

    Delete update-system-prompt-v2 (1) directory

--- update-system-prompt-v2 (1)/src/components/ApiKeyBanner.tsx
+++
@@ -1,4 +0,0 @@
-export default function ApiKeyBanner() {
-  return null;
-}
-//ad this comment anywhere
--- update-system-prompt-v2 (1)/src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App";
-
-const el = document.getElementById("root");
-if (el) {
-  createRoot(el).render(<App />);
-}
